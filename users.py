4666b142657417f2f84ca6f160cfb42c2f47fe4f
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2020-10-26 18:32:43 -0400

    Created function for getting and removing unauthorized sudoers

--- users.py
+++ users.py
@@ -3,7 +3,6 @@
 import os
 import sys
 
-
 # Helper function to read a file from a path
 def readfile(path):
     with open(path) as file:
@@ -34,6 +33,13 @@ def get_unauth_users(path):
     unauth = [u for u in current_users if u not in allowed_users]
     return unauth
 
+def get_non_admins(path,sudoers):
+    # Get list of allowed admins
+    allowed_admins = readfile(path).split('\n')
+    # Return list of users that are not in that list
+    return [ user for user in sudoers if user not in allowed_admins ]
+    
+    
 
 # Remove unauthorized users
 def remove_unauth(unauth):
@@ -46,7 +52,8 @@ def remove_unauth(unauth):
 
 
 def change_passwords(users):
-    current_user = shcmd("echo $USER")
+    # Strip newline character from echo command
+    current_user = shcmd("echo $USER").strip()
     for user in users:
         # Don't change the password for current user
         if user == current_user:
@@ -55,15 +62,39 @@ def change_passwords(users):
         cmd = "echo '{}:{}' | sudo chpasswd".format(user, "Password123#!")
         print "Changing password for {}".format(user)
         print '> ' + cmd
-        # stdout = shcmd(cmd)
-        # print stdout
+        stdout = shcmd(cmd)
+        print stdout
 
+# Gets the users that are capable of running sudo
+def get_sudoers(users):
+    sudoers = []
 
-# args = sys.argv
-# users_path = args[1]
+    for user in users:
+        cmd = "sudo -l -U {}".format(user)
+        stdout = shcmd(cmd)
+        if "not allowed" not in stdout:
+            sudoers.append(user)
+
+    return sudoers
+
+def remove_sudoers(non_admins):
+    for user in non_admins:
+        print "Removing admin privileges for: {}".format(user)
+        cmd = "sudo deluser {} sudo".format(user)
+        print "> " + cmd
+        stdout = shcmd(cmd)
+        print stdout
+
+args = sys.argv
+users_path = args[1]
+admin_users_path = args[2]
+
 # unauth = get_unauth_users(users_path)
-
 # remove_unauth(unauth)
 
 users = getusers()
-change_passwords(users)
+# change_passwords(users)
+
+current_admins=get_sudoers(users)
+
+non_admins = get_non_admins(admin_users_path,current_admins)
