4e4441ab81032f69578f48580e10ac8fa04148fa
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2020-10-27 17:39:23 -0400

    Added function to find suid binaries

--- programs.py
+++ programs.py
@@ -1,16 +1,21 @@
 from utils import shcmd, print_green, print_red, print_yellow
 import re
+import os
 
 
 def get_installed_programs(package_manager="apt"):
     installed = []
     if package_manager == "apt":
-        cmd = "apt list --installed"
+        cmd = "apt list --installed 2>/dev/null"
         stdout = shcmd(cmd)
         packages = stdout.split("\n")[1:]
         installed = [x.split("/")[0] for x in packages]
 
         return installed
+    if package_manager == "yum":
+        cmd = "yum list installed"
+        stdout = shcmd(cmd)
+        packages = stdout.split("\n")
 
 
 def get_acceptable_programs(installed):
@@ -18,7 +23,8 @@ def get_acceptable_programs(installed):
                            r'xdg.*', r'lib.*', 'firefox', r'chrom*.', r'fonts.*',
                            r'gcc.*', r'binutil.*', r'linux.*', r'manpages.*',
                            r'ncurses.*', r'systemd.*', r'vim.*', r'dconf.*',
-                           r'dpkg*', r'initra.*', r'glib.*', 'g\+\+.*']
+                           r'dpkg*', r'initra.*', r'glib.*', 'g\+\+.*', r'cloud.*',
+                           r'cpp.*', r'openssh.*']
     accepted_programs = []
     for pattern in acceptable_patterns:
         for program in installed:
@@ -33,8 +39,14 @@ def get_acceptable_programs(installed):
     return accepted_programs
 
 
+def find_suids():
+    print("SUID binaries on this machine:\n")
+    cmd = "find / -perm /4000 2>/dev/null"
+    os.system(cmd)
+
+
 if __name__ == "__main__":
     programs = get_installed_programs()
     accepted = get_acceptable_programs(programs)
-    for x in [p for p in programs if p not in accepted]:
-        print_yellow(x)
+    to_examine = [p for p in programs if p not in accepted]
+    find_suids()
