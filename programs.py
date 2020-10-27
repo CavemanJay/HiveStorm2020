ce59191610d3ce855e0cae0fafe06a2c50a44038
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2020-10-27 19:30:55 -0400

    Update programs script

--- programs.py
+++ programs.py
@@ -1,6 +1,7 @@
 from utils import shcmd, print_green, print_red, print_yellow
 import re
 import os
+from pprint import pprint
 
 
 def get_installed_programs(package_manager="apt"):
@@ -11,11 +12,18 @@ def get_installed_programs(package_manager="apt"):
         packages = stdout.split("\n")[1:]
         installed = [x.split("/")[0] for x in packages]
 
-        return installed
     if package_manager == "yum":
         cmd = "yum list installed"
         stdout = shcmd(cmd)
         packages = stdout.split("\n")
+
+    if package_manager == "pacman":
+        cmd = "pacman -Qqe"
+        stdout = shcmd(cmd)
+        packages = stdout.split("\n")
+        installed = packages
+
+    return installed
 
 
 def get_acceptable_programs(installed):
@@ -24,7 +32,8 @@ def get_acceptable_programs(installed):
                            r'gcc.*', r'binutil.*', r'linux.*', r'manpages.*',
                            r'ncurses.*', r'systemd.*', r'vim.*', r'dconf.*',
                            r'dpkg*', r'initra.*', r'glib.*', 'g\+\+.*', r'cloud.*',
-                           r'cpp.*', r'openssh.*']
+                           r'cpp.*', r'openssh.*', r'xorg.*', r'bash.*', r'dbus.*',
+                           r'gpg.*', r'gnupg.*', r'ubuntu.*']
     accepted_programs = []
     for pattern in acceptable_patterns:
         for program in installed:
@@ -49,4 +58,5 @@ if __name__ == "__main__":
     programs = get_installed_programs()
     accepted = get_acceptable_programs(programs)
     to_examine = [p for p in programs if p not in accepted]
-    find_suids()
+    pprint(to_examine)
+    # find_suids()
