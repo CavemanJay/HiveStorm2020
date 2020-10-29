f677f4456f3798211eb4935b14006b730513ff83
JayCuevas <Jordan.Cuevas@precisecal.com>
2020-10-28 23:26:12 -0700

    Added some basic logic operations Also added a function for getting a yes/no resposne from user

--- windows/policies.ps1
+++ windows/policies.ps1
@@ -7,7 +7,54 @@ param (
     # $verbose
 )
 
+function New-Section {
+    param (
+        [string]
+        $header
+    )
+    
+    Clear-Host
+    echo "============ $header ============" 
+}
+
+function Get-YesNo {
+    param (
+        [string]
+        $prompt
+    )
+
+    do {
+        $input = (Read-Host "$prompt (Y/N)").ToLower()
+    }
+    while ($input -notin @('y', 'n'))
+    return $input
+}
+
 $isVerbose = [bool]$PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent
 
-Write-Output "Enabling firewall..."
-Set-NetFirewallProfile -Enabled True -Verbose:$isVerbose
+
+New-Section "Firewall"
+
+$rules = Get-NetFirewallProfile
+$disabled = $false
+
+foreach ($rule in $rules) {
+    Write-Verbose "Firewall Profile: $($rule.Name) | Enabled: $($rule.Enabled)"
+    if (!$rule.Enabled) {
+        $disabled = $true
+    }
+}
+
+
+if ($disabled) {
+    $enableFirewall = Get-YesNo "Firewall is disabled. Would you like to enable it?"
+    if ($enableFirewall -eq "y") {
+        Write-Output "Enabling firewall"
+        Set-NetFirewallProfile -Enabled True -Verbose:$isVerbose
+    }
+}
+else {
+    echo "Firewall is enabled for all profiles"
+}
+
+# New-Section -header "Test"
